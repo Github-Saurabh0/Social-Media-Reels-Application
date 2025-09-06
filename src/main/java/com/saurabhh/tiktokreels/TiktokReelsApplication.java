50b01b559152730b4098637a898b34873d379e61
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-09-06 12:06:02 +0530

    initialize TiktokReels Spring Boot application

---
+++ src/main/java/com/saurabhh/tiktokreels/TiktokReelsApplication.java
@@ -0,0 +1,13 @@
+package com.saurabhh.tiktokreels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiktokReelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiktokReelsApplication.class, args);
+	}
+
+}
